0ee9add2aca846e1fbf0d1e134283d098d1b1f9b
Haakon Kavli <haakon.kavli@prescient.co.za>
2016-02-12 10:39:41 +0200

    No change, just moved directory

--- R/fillts.R
+++ R/fillts.R
@@ -1,9 +1,12 @@
 fillts <- function(zooDaily){
         startDate <- index(zooDaily)[1]
         endDate <- index(zooDaily)[length(index(zooDaily))]
-        allDays <- seq(as.Date(startDate),as.Date(endDate),by = 'day')
-        newZoo <- zoo(data.frame(zooDaily),order.by = allDays)
+        allDays <- data.table(datestamp = seq(as.Date(startDate),as.Date(endDate),by = 'day'))
+        setkey(allDays,'datestamp')
+        oldZoo.dt <- data.table(datestamp = index(zooDaily), zooDaily)
+        setkey(oldZoo.dt,'datestamp')
+        newZoo.df <- data.frame(oldZoo.dt[allDays])
+        newZoo <- zoo(newZoo.df[,-1],order.by = newZoo.df[,1])
         filledDailyZoo <- na.locf(newZoo)
-
         return(filledDailyZoo)
 }
